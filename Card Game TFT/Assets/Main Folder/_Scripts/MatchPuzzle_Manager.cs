78cba137d9092f22b0f39a4d956a6729718cabb3
Harby2626 <hasancangormez26@gmail.com>
2023-02-14 18:01:12 +0300

    two sided shader
    added two sided shader and puzzle UI initial scripts

---
+++ Card Game TFT/Assets/Main Folder/_Scripts/MatchPuzzle_Manager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MatchPuzzle_Manager : MonoBehaviour
+{
+    List<GameObject> ChosenCards = new List<GameObject>();
+
+    public void ChoseCard()
+    {
+        if (!ChosenCards.Count.Equals(0)) // if any card has already chosen
+        {
+
+        }
+    }
+}
