8f74d0704a46112799277417abb960958aa88470
Harby2626 <hasancangormez26@gmail.com>
2023-02-15 18:03:18 +0300

    card chosing anim
    card chosing animation added and card dealing inital build-up

---
+++ Card Game TFT/Assets/Main Folder/_Scripts/SO/AllPuzzleCards_SO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "All Puzzle Cards")]
+public class AllPuzzleCards_SO : ScriptableObject
+{
+    public List<GameObject> AllPuzzleCards = new List<GameObject>();
+}
