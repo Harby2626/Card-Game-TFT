304bd37aa5ffca837219b129559db7f51af60a90
Harby2626 <hasancangormez26@gmail.com>
2023-02-16 14:56:08 +0300

    puzzle card not match mech - puzzle pop_up
    puzzle cards dealing added, puzzle pop-up added, puzzle cards not match mechanic added

--- Card Game TFT/Assets/Main Folder/_Scripts/Manager Scripts/DealingCardManager.cs
+++ Card Game TFT/Assets/Main Folder/_Scripts/Manager Scripts/DealingCardManager.cs
@@ -2,18 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DealingCardManager : MonoBehaviour
 {
-    MatchPuzzle_Manager matchPuzzleManager;
+    public int counter = 0;
+    float timer = 0f, card_deal_wait = .25f;
+
+    MatchPuzzle_Manager puzzleMng;
     private void Start()
     {
-        matchPuzzleManager = GameObject.Find("Match Puzzle Manager").GetComponent<MatchPuzzle_Manager>();
+        puzzleMng = GameObject.Find("Match Puzzle Manager").GetComponent<MatchPuzzle_Manager>();
+    }
+
+    IEnumerator DealCards()
+    {
+        yield return new WaitForSeconds(1.1f);
+        if (puzzleMng.AvailableCardSlots.Count > 0)
+        {
+
+            timer += Time.deltaTime;
+            if (timer > card_deal_wait)
+            {
+                int randVal = Random.Range(0, puzzleMng.AvailableCardSlots.Count);
+                DealCard(counter, randVal);
+                counter++;
+                if (counter == puzzleMng.CardsOnPuzzle.Count)
+                {
+                    puzzleMng.puzzlePhase = MatchPuzzle_Manager.PuzzlePhase.Playing;
+                    puzzleMng.ActivateCardButtons();
+                }
+                timer = 0f;
+            }
+        }
     }
 
     private void Update()
     {
+        StartCoroutine(DealCards());
+        //if (puzzleMng.AvailableCardSlots.Count > 0)
+        //{
+
+        //    timer += Time.deltaTime;
+        //    if (timer > card_deal_wait)
+        //    {
+        //        int randVal = Random.Range(0, puzzleMng.AvailableCardSlots.Count);
+        //        DealCard(counter, randVal);
+        //        counter++;
+        //        if (counter == puzzleMng.CardsOnPuzzle.Count)
+        //        {
+        //            puzzleMng.puzzlePhase = MatchPuzzle_Manager.PuzzlePhase.Playing;
+        //            puzzleMng.ActivateCardButtons();
+        //        }
+        //        timer = 0f;
+        //    }
+        //}
 
     }
+
+
+    void DealCard(int index, int slot_index)
+    {
+        puzzleMng.CardsOnPuzzle[index].GetComponent<PuzzleCard>().CardSlotTarget = puzzleMng.AvailableCardSlots[slot_index].GetComponent<RectTransform>().anchoredPosition;
+        puzzleMng.CardsOnPuzzle[index].GetComponent<PuzzleCard>().setToTarget = true;
+        puzzleMng.AvailableCardSlots.Remove(puzzleMng.AvailableCardSlots[slot_index]);
+    }
+
 
 }
--- Card Game TFT/Assets/Main Folder/_Scripts/Manager Scripts/MatchPuzzle_Manager.cs
+++ Card Game TFT/Assets/Main Folder/_Scripts/Manager Scripts/MatchPuzzle_Manager.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MatchPuzzle_Manager : MonoBehaviour
 {
+    //float timer = 0f, closeWaitTime = 1f;
 
 
     [SerializeField] AllPuzzleCards_SO AllPuzzleCards;
@@ -14,14 +18,13 @@ public class MatchPuzzle_Manager : MonoBehaviour
     GameObject cardSpawnPos;
 
     // This is a list for checking current match checking
-    List<PuzzleCard> ChosenCards = new List<PuzzleCard>();
+    List<GameObject> ChosenCards = new List<GameObject>();
 
-    enum PuzzlePhase
+    public enum PuzzlePhase
     {
-        Card_Dealing,
-        Playing
+        Card_Dealing, Halt, Playing
     }
-    PuzzlePhase puzzlePhase;
+    public PuzzlePhase puzzlePhase;
 
     private void Start()
     {
@@ -32,33 +35,48 @@ public class MatchPuzzle_Manager : MonoBehaviour
         MoveAmount = 8;
     }
 
+    IEnumerator HandleMatch()
+    {
+        yield return new WaitForSeconds(1f);
+        if(ChosenCards.Count == 2)
+        {
+            MoveAmount--;
+            if (ChosenCards[0].GetComponent<PuzzleCard>().Card.cardType == ChosenCards[1].GetComponent<PuzzleCard>().Card.cardType)// When cards matches
+            {
+                DeactivateCardButtons();
+                ChosenCards.Clear();
+                ActivateCardButtons();
+                Debug.Log("MATCH!");
+
+            }
+
+            else// Cards doesn't match
+            {
+                DeactivateCardButtons();
+                CloseCard(ChosenCards[0]); CloseCard(ChosenCards[1]);
+                ChosenCards.Clear();
+                ActivateCardButtons();
+                Debug.Log("TRY AGAIN");
+            }
+        }
+    }
+
+
     public void ChoseCard()// This function choses a card when selected, and plays turn animation adds chosen cards to List
     {
         if (MoveAmount > 0)
         {
             GameObject thisCardGO = EventSystem.current.currentSelectedGameObject; // Chosen card gameObject
-            PuzzleCard selectedCard = thisCardGO.GetComponent<PuzzleCard>(); // Chosen card --PuzzleCard-- script
 
             if (ChosenCards.Count < 2) // If player doesn't choose two cards
             {
-                ChosenCards.Add(selectedCard);
+                ChosenCards.Add(thisCardGO);
+                thisCardGO.GetComponent<PuzzleCard>().state = PuzzleCard.State.Opening;
                 thisCardGO.GetComponent<Animator>().SetBool("chosen", true);
             }
 
-            if (ChosenCards.Count == 2) // If player selected two card
-            {
-                MoveAmount--;
-                if (ChosenCards[0].Card.cardType == ChosenCards[1].Card.cardType)// When cards matches
-                {
-                    Debug.Log("MATCH!");
+            StartCoroutine(HandleMatch());
 
-                }
-
-                else// Cards doesn't match
-                {
-                    Debug.Log("TRY AGAIN");
-                }
-            }
         }
         
     }
@@ -66,13 +84,11 @@ public class MatchPuzzle_Manager : MonoBehaviour
     private void Update()
     {
 
-
         switch (puzzlePhase)
         {
             case PuzzlePhase.Card_Dealing:
-                // Deal the cards from dealing pos to card slots randomly
                 SpawnCards();
-                puzzlePhase = PuzzlePhase.Playing;
+                puzzlePhase = PuzzlePhase.Halt;
                 break;
             case PuzzlePhase.Playing:
                 //Playing phase (time runs out 15s (?))
@@ -109,4 +125,32 @@ public class MatchPuzzle_Manager : MonoBehaviour
         }
 
     }
+    void HandleMatching()
+    {
+
+    }
+
+
+    public void ActivateCardButtons()
+    {
+        foreach (GameObject item in CardsOnPuzzle)
+        {
+            item.GetComponent<Button>().enabled = true;
+        }
+    }
+
+    public void DeactivateCardButtons()
+    {
+        foreach (GameObject item in CardsOnPuzzle)
+        {
+            item.GetComponent<Button>().enabled = false;
+        }
+    }
+
+    void CloseCard(GameObject card)
+    {
+        card.GetComponent<Animator>().SetBool("chosen", false);
+        card.GetComponent<Animator>().SetBool("wrong", true);
+    }
+
 }
